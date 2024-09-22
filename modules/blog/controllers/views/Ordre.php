e205153ad6ee00cf842521985caf0b7a57533465
Elliot Lartigaud <elliot.lartigaud@etu.univ-amu.fr>
2024-09-22 02:11:52 +0200

    maj

--- modules/blog/controllers/views/Ordre.php
+++
@@ -1,8 +0,0 @@
-
-<?php
-	require 'utils/helpers.php';
-    $pages = array('Ordre','Accueil','compte');
-    start_page($pages);
-    end_page();
-	
-?>
--- modules/blog/controllers/views/compte.php
+++
@@ -1,7 +0,0 @@
-<?php
-	require 'utils/helpers.php';
-    $pages = array('Accueil','Ordre','compte');
-    start_page($pages);
-    end_page();
-	
-?>
