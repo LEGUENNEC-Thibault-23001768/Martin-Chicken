7ea4486f8bc346e9194e377b84b3527ac4197f55
Batman <joud.chebira@etu.univ-amu.fr>
2024-09-26 17:19:56 +0200

    Login

---
+++ modules/Controleurs/CompteController.php
@@ -0,0 +1,49 @@
+<?php
+final class CompteController
+{
+    public static string $titre  = "Connexion";
+    public function defaultAction()
+    {
+        if (AuthModel::isLoggedIn()) {
+            header("Location: /?ctrl=Plat"); // redirect to dashboard
+        } else {
+            $error = isset($_SESSION['login_error']) ? $_SESSION['login_error'] : '';
+            unset($_SESSION['login_error']);
+            Vue::montrer('compte', array('error' => $error));
+        }
+    }
+
+    public function loginAction() {
+
+        if ($_SERVER['REQUEST_METHOD'] !== 'POST')
+            return;
+
+        if (isset($_SESSION['login_error']))
+            unset($_SESSION['login_error']);
+
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+
+        $auth_model = new AuthModel();
+        
+        $result = $auth_model->login($username, $password);
+
+        try {
+
+            if (!$result) {
+                $_SESSION['login_error'] = "Mauvais nom d'utilisateur ou mot de passe";
+                header('Location: index.php?ctrl=Compte');
+                return;
+            }
+
+            header('Location: index.php?ctrl=Repas'); // rediriger vers dashboard
+
+
+        } catch (Exception $e) {
+            $err = $e->getMessage();
+            $_SESSION['login_error'] = $err;
+            header('Location: index.php?ctrl=Login');
+        }
+    }
+
+}
---
+++ modules/Vues/formulaires/login.php
@@ -0,0 +1,23 @@
+    <div id="cadre">
+        <h2>Connection</h2>
+        <form action="?ctrl=Compte&action=login" method="post">
+            
+            <section class="eleForm">
+                <label for="username">Nom d'utilisateur</label>
+                <input type="text" id="username" name="username" placeholder="Entrez votre nom d'utilisateur" required>
+            </section>
+            
+            <section class="eleForm">
+                <label for="password">Mot de passe</label>
+                <input type="password" id="password" name="password" placeholder="Entrez votre mot de passe" required>
+            </section>
+            
+            <section class="eleForm">
+                <a href="#">Mot de passe oublié ?</a>
+            </section>
+            
+            <section class="eleForm">
+                <button type="submit" class="login-btn">Se connecter</button>
+            </section>
+        </form>
+    </div>
